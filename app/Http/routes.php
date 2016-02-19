d89c005720fc7cc94030eea054f8012b6a0beaab
Abhay <abhayrawat2695@gmail.com>
2016-02-19 21:59:19 +0530

    spellings corrected

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,8 +7,10 @@ Route::group(['middleware' => ['web']], function () {
 	Route::get('login',array('as'=>'login','uses'=>'PagesController@login'));
 	Route::get('logout',array('as'=>'logout','uses'=>'PagesController@logout'));
 	Route::get('signup',array('as'=>'signup','uses'=>'PagesController@signup'));
-	Route::put('signupstep1',array('brfore'=>'csrf','uses'=>'PagesController@signupstep1'));
-	Route::put('signupstep2',array('brfore'=>'csrf','uses'=>'PagesController@signupstep2'));
-	Route::put('userlogin',array('brfore'=>'csrf','uses'=>'PagesController@userlogin'));
-
+	Route::put('signupstep1',array('before'=>'csrf','uses'=>'PagesController@signupstep1'));
+	Route::put('signupstep2',array('before'=>'csrf','uses'=>'PagesController@signupstep2'));
+	Route::put('userlogin',array('before'=>'csrf','uses'=>'PagesController@userlogin'));
+	Route::get('/', function(){
+		return 'root';
+	});
 });
