c7103ff5851f1ddb1a82fd6443e2772a80f17348
Abhay <abhayrawat2695@gmail.com>
2016-02-19 22:01:01 +0530

    groups made

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,11 +6,18 @@ Route::group(['middleware' => ['web']], function () {
 
 	Route::get('login',array('as'=>'login','uses'=>'PagesController@login'));
 	Route::get('logout',array('as'=>'logout','uses'=>'PagesController@logout'));
-	Route::get('signup',array('as'=>'signup','uses'=>'PagesController@signup'));
-	Route::put('signupstep1',array('before'=>'csrf','uses'=>'PagesController@signupstep1'));
-	Route::put('signupstep2',array('before'=>'csrf','uses'=>'PagesController@signupstep2'));
-	Route::put('userlogin',array('before'=>'csrf','uses'=>'PagesController@userlogin'));
 	Route::get('/', function(){
 		return 'root';
 	});
+	Route::get('signup',array('as'=>'signup','uses'=>'PagesController@signup'));
+
+
+
+	Route::put('signupstep1',array('before'=>'csrf','uses'=>'PagesController@signupstep1'));
+	Route::put('signupstep2',array('before'=>'csrf','uses'=>'PagesController@signupstep2'));
+	Route::put('userlogin',array('before'=>'csrf','uses'=>'PagesController@userlogin'));
+
+
+
+	
 });
